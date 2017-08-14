1ba5d77feeb7b318c1e1338af38b076bbd11d413
Izabel Lima <belica18@gmail.com>
2017-08-14 19:13:39 -0300

    css style done

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
-  if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var lon = parseFloat(position.coords.longitude).toFixed(2);
-    var lat = parseFloat(position.coords.latitude).toFixed(2);
-    var jsonaddress = "https://fcc-weather-api.glitch.me/api/current?lon=" + lon + "&lat=" + lat;
-    $.getJSON(jsonaddress, function(data) {
-    console.log(data.weather[0].main);
-    console.log(data.main.temp);
-    console.log(data.weather[0].icon);
-    console.log(data.name);
-    });
-  });
-};
-});
+// $(document).ready(function() {
+//   if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     var lon = parseFloat(position.coords.longitude).toFixed(2);
+//     var lat = parseFloat(position.coords.latitude).toFixed(2);
+//     var jsonaddress = "https://fcc-weather-api.glitch.me/api/current?lon=" + lon + "&lat=" + lat;
+//     $.getJSON(jsonaddress, function(data) {
+//     console.log(data.weather[0].main);
+//     console.log(data.main.temp);
+//     console.log(data.weather[0].icon);
+//     console.log(data.name);
+//     });
+//   });
+// };
+// });
 
 
